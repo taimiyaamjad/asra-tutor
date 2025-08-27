54f9f7c2a50144a09a75c0fd0258ef15f56185a3
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-08-27 14:22:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <PublicHeader>
    
    wi

--- src/components/public-header.tsx
+++ src/components/public-header.tsx
@@ -17,7 +17,7 @@ export function PublicHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-12 items-center">
         <div className="mr-4 flex">
           <Link href="/home" className="flex items-center space-x-2">
             <BrainCircuit className="h-6 w-6 text-primary" />
