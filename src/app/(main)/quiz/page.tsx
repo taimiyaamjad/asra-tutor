fe1b218cbc3fda2c24336e60b783c89361df055c
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-09-06 15:21:56 +0000

    First change the limit in quiz generation to 5,10,25,50,100

--- src/app/(main)/quiz/page.tsx
+++ src/app/(main)/quiz/page.tsx
@@ -165,7 +165,9 @@ export default function QuizPage() {
                 <SelectContent>
                   <SelectItem value="5">5</SelectItem>
                   <SelectItem value="10">10</SelectItem>
-                  <SelectItem value="15">15</SelectItem>
+                  <SelectItem value="25">25</SelectItem>
+                  <SelectItem value="50">50</SelectItem>
+                  <SelectItem value="100">100</SelectItem>
                 </SelectContent>
               </Select>
             </div>
