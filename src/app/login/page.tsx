b000d71e6d51479c5395791ad1fb3905e5d62f31
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-09-12 13:32:27 +0000

    One more thing like when someone came to the homepage and then click log

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,49 @@
+'use client';
+
 import { UserAuthForm } from '@/components/user-auth-form';
 import { BrainCircuit } from 'lucide-react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/lib/firebase';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push('/dashboard');
+    }
+  }, [user, router]);
+
+  if (loading || user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <svg
+          className="mr-2 h-16 w-16 animate-spin"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    );
+  }
+
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center animate-fade-in px-4 sm:px-0">
       <div className="lg:p-8">
